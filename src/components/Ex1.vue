f9d868dd26b0906288ab28d15a3cbd965339abfe
angelnr3 <angel.tan.2024@computing.smu.edu.sg>
2026-09-11 12:23:05 +0800

    Further update Week 4 Exercises

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -3,13 +3,13 @@
 
     // Add code here
     // challenge: use array  
-    const message = ref("Choose your favourite: ");
+    const message = ref("Choose your favorite: ");
     const fruits = ref(["apple", "orange", "peach"]);
     
 </script>
 
 <template>
-    <p>{{ message }} </p>
+    <p>{{ message }}</p>
    
     <!--<label for=""> <input type="radio" name="fruit"> {{ fruits[0] }} </label> 
     this method requires manual additions per fruit via each index [1], [2]
